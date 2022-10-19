3b2cca97f77643d237736fd8be7ea51a6d042eb9
malik H <malik@yahoo.fr>
2022-10-19 12:20:25 +0200

    exo 2 insert

--- 05-enregistrement.sql
+++ 05-enregistrement.sql
@@ -42,3 +42,35 @@ dt_creation 01/01/2020
 nom Béatrice  
 age 20
 dt_creation 01/02/2020
+
+-- solution 1 : 2 requêtes INSERT
+INSERT INTO etudiant 
+(nom , age , dt_creation) 
+VALUES 
+("Alain" , 22 , "2022-01-01" ) ;
+
+INSERT INTO etudiant 
+(nom , age , dt_creation) 
+VALUES 
+("Béatrice" , 20 , "2022-02-01" ) ;
+
+-- solution 1 : 1 requête INSERT
+
+INSERT INTO etudiant 
+(nom , age , dt_creation) 
+VALUES 
+("Alain" , 22 , dt_creation ) ,
+("Béatrice" , 20 , "2022-02-01" ) ; 
+
+SELECT * FROM etudiant ;
+
+-- exactement la même syntaxe sur mySQL / mariadb / sqlite 
+
+-- uniquement sur MySQL 
+INSERT INTO etudiant SET nom = "Alain" , age = 22 , dt_creation = "2022-02-01" ;
+
+-- il est possible d'utiliser la requête INSERT INTO sans la première pair de parenthèses
+-- connaitre l'id 
+-- respecter l'ordre des colonnes 
+INSERT INTO etudiant VALUES (3 , "Charles" , 45, "2022-02-01") ;
+-- exactement la même syntaxe sur mySQL / mariadb / sqlite 
