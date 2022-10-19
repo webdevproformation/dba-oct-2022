d61c2f61ab29428c7bf646945f565a4c2b65cf99
malik H <malik@yahoo.fr>
2022-10-19 14:18:36 +0200

    SELECT WHERE

--- 05-read.sql
+++ 05-read.sql
@@ -50,3 +50,83 @@ Ajouter des lignes dedans
 
 -- bon appétit rdv 13h30 !!! @ toute suite !!!
 -- https://github.com/webdevproformation/dba-oct-2022
+
+-- utiliser les valeurs stockées dans une table 
+-- récupérer toutes les salles sur les attributs (colonnes) suivants 
+SELECT nom , capacite , dt_creation 
+FROM salle ; 
+
+-- année
+SELECT nom , capacite , STRFTIME(  "%Y" ,  dt_creation  )
+FROM salle ;
+-- mois
+SELECT nom , capacite , STRFTIME(  "%m" ,  dt_creation  )
+FROM salle ;
+-- jour
+SELECT nom , capacite , STRFTIME(  "%d" ,  dt_creation  )
+FROM salle ;
+
+-- jj/mm/aaaa
+SELECT nom , capacite , STRFTIME(  "%d/%m/%Y" ,  dt_creation  )
+FROM salle ;
+
+
+-- renommer une colonne dans le résultat dans la requete SELECT 
+-- AS utiliser comme Alias de colonne => 
+-- AS est aussi utilisé sur les tables notamment pour les jointures
+SELECT nom , capacite , STRFTIME(  "%d/%m/%Y" ,  dt_creation  ) AS `date française`
+FROM salle ;
+
+-- alias de table 
+SELECT s.nom , s.capacite , STRFTIME(  "%d/%m/%Y" ,  s.dt_creation  ) AS `date française`
+FROM salle AS s;
+
+-- filtrage / recherche WHERE 
+
+-- afficher toutes les salles dont la capacite >= 22 places
+
+SELECT * 
+FROM salle 
+WHERE capacite >= 22 ; 
+
+-- toutes les salles qui sont localisées à Marseille
+
+SELECT * 
+FROM salle 
+WHERE localisation = "MArseille" ; 
+
+-- retrouver toutes salles dont le nom contient la lettre o 
+
+SELECT * 
+FROM salle 
+WHERE nom LIKE "%o%" ;
+
+-- retrouver toutes salles dont le nom commence par r
+
+SELECT * 
+FROM salle 
+WHERE nom LIKE "r%" ;
+
+-- afficher toutes les salles dont la localisation finie par le lettre s
+
+SELECT * 
+FROM salle 
+WHERE localisation LIKE "%s" ;
+
+-- retrouver toutes salles dont le nom contient les lettres "ou"  "OU" 
+
+SELECT * 
+FROM salle 
+WHERE nom LIKE "%oU%" ;
+
+
+SELECT * 
+FROM salle 
+WHERE LOWER(localisation) = "paris" ;
+
+
+-- récupérer toutes les réservations qui ont été réalisées en Janvier 
+
+SELECT * 
+FROM salle 
+WHERE STRFTIME("%m" , dt_creation) = "01" ;
