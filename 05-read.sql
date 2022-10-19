fdee6efa8c5cd87cc890147d934a653101479a5b
malik H <malik@yahoo.fr>
2022-10-19 12:24:07 +0200

    exo 2 select

---
+++ 05-read.sql
@@ -0,0 +1,15 @@
+créer une table salle
+
+id PK
+nom texte maximum 50 lettres
+capacite int Default 10
+localisation maximum 50 lettres par défaut "Paris"
+dt_creation DATETIME Defaut maintenant
+
+
+Ajouter des lignes dedans
+
+"jaune" 30 Paris maintenant
+"verte" 10 Marseille 01/01/2022 11:00
+"rouge" 15 Paris 02/01/2022 10:00
+"rose"   5 Paris maintenant
