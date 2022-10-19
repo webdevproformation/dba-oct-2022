2a672d66bb6b35c202e466c70379e690d544c45a
malik H <malik@yahoo.fr>
2022-10-19 13:47:10 +0200

    correction

--- 05-read.sql
+++ 05-read.sql
@@ -5,7 +5,41 @@ nom texte maximum 50 lettres
 capacite int Default 10
 localisation maximum 50 lettres par défaut "Paris"
 dt_creation DATETIME Defaut maintenant
+DROP TABLE salle ;
+CREATE TABLE salle(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    nom VARCHAR(50) ,
+    capacite INT DEFAULT 10 ,
+    localisation VARCHAR(50) DEFAULT "Paris",
+    dt_creation DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
+-- SQlite 
+INSERT INTO salle 
+(nom , capacite , localisation , dt_creation)
+VALUES 
+("jaune" , 30 , "Paris" , DATETIME('now','localtime')),
+("verte" , 10 , "Marseille" , "2022-01-01 11:00:00"),
+("rouge" , 15 , "Paris" , "2022-01-02 10:00:00"),
+("rose" ,   5 , "Paris" , DATETIME('now','localtime'));
+-- INSERT (4 colonnes) => VALUES il faut avoir 4 valeurs pas possible avec cette syntaxe
+
+
+-- MySQL MariaDB
+INSERT INTO salle 
+(nom , capacite , localisation , dt_creation)
+VALUES 
+("jaune" , 30 , "Paris" , NOW()),
+("verte" , 10 , "Marseille" , "2022-01-01 11:00:00"),
+("rouge" , 15 , "Paris" , "2022-01-02 10:00:00"),
+("rose" ,   5 , "Paris" , NOW());
+
+
+INSERT INTO salle 
+(nom , capacite )
+VALUES 
+("jaune" , 30 ) ; 
+-- avec cette syntaxe vous pouvez utiliser les valeurs par défaut 
 
 Ajouter des lignes dedans
 
@@ -13,3 +47,6 @@ Ajouter des lignes dedans
 "verte" 10 Marseille 01/01/2022 11:00
 "rouge" 15 Paris 02/01/2022 10:00
 "rose"   5 Paris maintenant
+
+-- bon appétit rdv 13h30 !!! @ toute suite !!!
+-- https://github.com/webdevproformation/dba-oct-2022
